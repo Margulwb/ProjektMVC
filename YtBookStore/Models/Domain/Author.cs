9c5e19442343e7e48db740cdf41386f475cccbbc
Margul <margulwb@gmail.com>
2023-02-19 08:35:00 +0100

    init

---
+++ YtBookStore/Models/Domain/Author.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YtBookStore.Models.Domain
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string AuthorName { get; set; }
+    }
+}
